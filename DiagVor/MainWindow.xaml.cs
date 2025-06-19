9fe67b9ad889536a4a837e2bb47e30b6b06a71fb
NasyaGergil <gergilmo11@gmail.com>
2025-06-19 13:54:49 +0300

    added the implementation of creating and selecting buttons with the mouse

--- DiagVor/MainWindow.xaml.cs
+++ DiagVor/MainWindow.xaml.cs
@@ -1,12 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DiagVor
@@ -16,9 +13,85 @@ namespace DiagVor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double PointRadius = 3;
+        private List<Ellipse> points;
+
         public MainWindow()
         {
             InitializeComponent();
+            points = new List<Ellipse>();
+        }
+
+        private void BuildVoronoi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point clickPosition = e.GetPosition(Canvas);
+
+            Ellipse clickedPoint = null;
+            foreach (var point in points)
+            {
+                Point pointCenter = new Point(
+                    Canvas.GetLeft(point) + PointRadius,
+                    Canvas.GetTop(point) + PointRadius
+                );
+
+                double distance = Math.Sqrt(
+                    Math.Pow(clickPosition.X - pointCenter.X, 2) +
+                    Math.Pow(clickPosition.Y - pointCenter.Y, 2)
+                );
+
+                if (distance <= PointRadius)
+                {
+                    clickedPoint = point;
+                    break;
+                }
+            }
+
+            if (clickedPoint != null)
+            {
+                Canvas.Children.Remove(clickedPoint);
+                points.Remove(clickedPoint);
+                NumPoints.Text = points.Count.ToString();
+            }
+            else
+            {
+                var point = new Ellipse
+                {
+                    Width = PointRadius * 2,
+                    Height = PointRadius * 2,
+                    Fill = Brushes.Blue,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1
+                };
+
+                Canvas.SetLeft(point, clickPosition.X - PointRadius);
+                Canvas.SetTop(point, clickPosition.Y - PointRadius);
+
+                Canvas.Children.Add(point);
+                points.Add(point);
+                NumPoints.Text = points.Count.ToString();
+            }
+        }
+
+        private void ClearPoints_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Children.Clear();
+            points.Clear();
+            NumPoints.Text = "0";
+        }
+
+        private void GenerateSingleThread_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateMultiThread_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
